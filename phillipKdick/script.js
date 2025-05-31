01803ad5c50f94bbd28fb31ae7e6a3b9715f7239
ropetightightrope <santino.castagna@gmail.com>
2025-05-31 21:49:23 +1000

    FUCK

---
+++ phillipKdick/script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById(canvas1);
+const ctx = canvas.getContext('id');
+const CANVAS_WIDTh = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 800;
+let gameSpeed = 5;
---
+++ pkd/script.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById(canvas1);
+const ctx = canvas.getContext('id');
+const CANVAS_WIDTh = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 800;
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'layer-1.png';
+const backgroundLayer2 = new Image ();
+backgroundLayer2.sc = 'layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.sc ='layer-3.png'
+const backgroundLayer4 = new Image ();
+backgroundLayer4.sc = 'layer-4.png';
+const backgroundLayer5 = new Image ();
+backgroundLayer5.sc ='layer-5.png';
+
+
+function animate(){
+drawImage(backgroundLayer1, 0, 0);
+
+
+}
